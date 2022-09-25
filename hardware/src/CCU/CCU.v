b85d2ecaf082bed038f70ba97d358ae124156ef6
zhouchch3 <2835079913@qq.com>
2022-09-25 23:09:53 +0800

    feat(CCU.v): Configure RAM, addr, FSM, output

--- hardware/src/CCU/CCU.v
+++ hardware/src/CCU/CCU.v
@@ -18,11 +18,12 @@ module CCU #(
     )(
     input                               clk                     ,
     input                               rst_n                   ,
+    input                               TOPCCU_start,
 
     // Configure
 input  ITFCCU_Dat             
 input  ITFCCU_DatVld          
-output ITFCCU_DatRdy          
+output CCUITF_DatRdy          
 output CCUSYA_CfgMod          
 output CCUSYA_CfgScale        
 output CCUSYA_CfgShift        
@@ -36,7 +37,10 @@ output CCUCTR_CfgNop
 output CCUCTR_CfgK            
 output CCUGLB_CfgVld          
 input  GLBCCU_CfgRdy          
-output CCUGLB_CfgPort_BankFlg 
+output CCUGLB_CfgBankPort 
+output CCUGLB_CfgPortMod,
+output CCUGLB_CfgRdPortLoop,
+output CCUGLB_CfgWrPortLoop,
 output CCUGLB_CfgPort_AddrMax 
 output CCUGLB_CfgRdPortParBank
 output CCUGLB_CfgWrPortParBank
@@ -48,16 +52,29 @@ output CCUGLB_Port_rst
 // Constant Definition :
 //=====================================================================================================================
 localparam IDLE    = 3'b000;
-localparam RD_CFG     = 3'b001;
-localparam LY_CFG    = 3'b010;
-localparam LY_WORK    = 3'b011;
-localparam FNH = 3'b100;
+localparam RD_CFG  = 3'b001;
+localparam LY_CFG  = 3'b010;
+localparam LY_WORK = 3'b011;
+localparam FNH     = 3'b100;
 
+
+localparam OpCode_Array = 3'd0;
+localparam OpCode_Conv  = 3'd1;
+localparam OpCode_FC    = 3'd2;
+localparam OpCode_Pool  = 3'd3;
+localparam OpCode_FPS   = 3'd4;
+localparam OpCode_KNN   = 3'd5;
+
+localparam Word_Array = 1;
+localparam Word_Conv  = 2;
+localparam Word_FC    = 1;
+localparam Word_Pool  = 2;
+localparam Word_FPS   = 1;
+localparam Word_KNN   = 1;
 //=====================================================================================================================
 // Variable Definition :
 //=====================================================================================================================
-wire                                start_COMP                       ;
-wire [ 6                    -1 : 0] MEM_CCUGB_block[0 : NUM_PEB -1 ];
+
 //=====================================================================================================================
 // Logic Design 1: FSM
 //=====================================================================================================================
@@ -66,19 +83,29 @@ reg [ 3     -1 : 0] state       ;
 reg [ 3     -1 : 0] next_state  ;
 always @(*) begin
     case ( state )
-        IDLE : if( ASICCCU_start)
-                    next_state <= RD_CFG; //A network config a time
-                else
-                    next_state <= IDLE;
-        RD_CFG: if( fifo_full)
-                    next_state <= LY_CFG;
-                else
-                    next_state <= RD_CFG;
-        LY_CFG: if( all_finish) /// COMP_FRM COMP_PAT COMP_...
-                    next_state <= IDLE;
-                else
-                    next_state <= LY_CFG;
-        default: next_state <= IDLE;
+        IDLE    :   if( TOPCCU_start)
+                        next_state <= RD_CFG; //
+                    else
+                        next_state <= IDLE;
+        RD_CFG  :   if( fifo_full) // 
+                        next_state <= LY_CFG;
+                    else
+                        next_state <= RD_CFG;
+        IDLE_CFG:   if (Array_ReqCfg) begin
+                        next_state <= ARRAY_CFG;
+                    end else if (Conv_ReqCfg) begin
+                        next_state <= CONV_CFG;
+                    end
+        ARRAY_CFG: 
+        CONV_CFG:   if( Conv_AckCfg) /// Every type layer cfg: Array, FC,...
+                        next_state <= IDLE_CFG;
+                    else
+                        next_state <= CONV_CFG;
+        FC_CFG  :
+        POL_CFG :
+        FPS_CFG :
+        KNN_CFG :
+        default:    next_state <= IDLE;
     endcase
 end
 always @ ( posedge clk or negedge rst_n ) begin
@@ -90,43 +117,127 @@ always @ ( posedge clk or negedge rst_n ) begin
 end
 
 //=====================================================================================================================
-// Logic Design 2: Addr Gen.
+// Logic Design 2: GLB Max Addr Gen.
 //=====================================================================================================================
 
-assign {
-    OpCode,
-    CCUSYA_CfgMod,
-    DatAddr,
-    WgtAddr,
-    Nip,
-    Chi,
-    Cho,
-    CCUSYA_CfgScale,
-    CCUSYA_CfgShift,
-    CCUSYA_CfgZp,
 
 
-    } = fifo_out;
+
+
+
+//=====================================================================================================================
+// Logic Design 3: Cfg Memory Address Gen: input ReqCfg, output AckCfg
+//=====================================================================================================================
+
+genvar i;
+generate
+    for(i=0; i<NUM_TYPE_LAYER; i=i+1) begin
+        always @(posedge clk or negedge rst_n)  begin
+            if (!rst_n) begin
+                addr[i] <= 0;
+            end else if ( state == IDLE | cnt_word == Word[i]) begin
+                addr[i] <= 0;
+            end else if ( ReqCfg & match) begin
+                addr[i] <= addr + 1;
+            end
+        end
+
+        always @(posedge clk or negedge rst_n)  begin
+            if (!rst_n) begin
+                cnt_word <= 0;
+            end else if (  cnt_word == Word[i]) begin
+                cnt_word <= 0;
+            end else if ( ReqCfg & match) begin
+                cnt_word <= cnt_word + 1;
+            end
+        end
+
+
+endgenerate
+
+
+always @(*) begin
+    for (j=0; j<NUM_TYPE_LAYER; j=j+1) begin
+        if (ReqCfg[j]) begin
+            addr_r = addr[j]
+        end
+    end
+end
+
+
+assign match = ;
+
+assign OpCode == data_out[0 +: 3];
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+    end else if ( read_en_d ) begin
+        if ( OpCode == OpCode_Array) begin
+            {Base_Addr0, Ly_Num, Mode} <= data_out[3 : PORT_WIDTH -1];
+        end else if ( OpCode == OpCode_Conv) begin
+            if (cnt_word == 0) begin
+                DatAddr <= data_out[3: 34];
+                WgtAddr <= data_out[35 :66];
+                Nip <= data_out[67 : 82];
+                Chi <= data_out[83 : 94];
+            end else if (cnt_word == 1) begin
+                Cho <= data_out[0 : 11];       
+                CCUSYA_CfgScale <= data_out[12 : 31];       
+                CCUSYA_CfgShift <= data_out[32 : 39];
+                CCUSYA_CfgZp <= data_out[40: 47];
+                CCUSYA_CfgMod <= data_out[48: 49];
+                WgtAddrRange <= data_out[50 : 65];
+                DatAddrRange <= data_out[66 : 81];
+                OfmAddrRange <= data_out[82 : 95]; // ?? =97
+            end
+        end else if (OpCode == OpCode_Pool) begin
+            
+        end else if (OpCode == OpCode_FC) begin
+            
+        end else if (OpCode == OpCode_FPS) begin
+            
+        end
+    end 
+end
 
 
 //=====================================================================================================================
 // Sub-Module :
 //=====================================================================================================================
 
-FIFO_FWFT #(
-    .DATA_WIDTH(SRAM_WIDTH ),
-    .ADDR_WIDTH(FIFO_ADDR_WIDTH )
-    ) U1_FIFO_FWFT_CFG(
-    .clk ( clk ),
-    .rst_n ( rst_n ),
-    .Reset ( 1'b0), 
-    .push(fifo_push) ,
-    .pop(fifo_pop ) ,
-    .data_in( IFCFG_data),
-    .data_out (fifo_out ),
-    .empty(fifo_empty ),
-    .full (fifo_full )
-    );
+RAM#(
+    .SRAM_BIT     ( PORT_WIDTH ),
+    .SRAM_BYTE    ( 1 ),
+    .SRAM_WORD    ( SRAM_WORD_ISA ),
+    .CLOCK_PERIOD ( CLOCK_PERIOD )
+)u_RAM_ISA(
+    .clk          ( clk          ),
+    .rst_n        ( rst_n        ),
+    .addr_r       ( addr_r       ),
+    .addr_w       ( addr_w       ),
+    .read_en      ( read_en      ),
+    .write_en     ( write_en     ),
+    .data_in      ( ITFCCU_Dat      ),
+    .data_out     ( data_out     )
+);
+// Write Path
+assign write_en = ITFCCU_DatVld & CCUITF_DatRdy;
+
+always @(posedge clk or rst_n) begin
+    if (!rst_n) begin
+        addr_w <= 0;
+    end else if (state == IDLE ) begin
+        addr_w <= 0;
+    end else if (write_en ) begin
+        addr_w <= addr_w + 1;
+    end
+end
+
+assign full = addr_w - min(addr[]) == SRAM_WORD_ISA;
+assign empty = addr_w == min(addr[];
+
+// Read Path
+assign read_en = ReqCfg;
+
 
 
 endmodule
