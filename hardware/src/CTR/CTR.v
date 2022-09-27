337a477fb23f7f602782e92c8e691f0117b6c55a
zhouchch3 <2835079913@qq.com>
2022-09-27 09:58:16 +0800

    feat(CTR.v): initialize first version

--- hardware/src/CTR/CTR.v
+++ hardware/src/CTR/CTR.v
@@ -21,8 +21,30 @@ module CTR #(
     input                               rst_n                   ,
 
     // Configure
-    input                               GBCFG_rdy               , // level
-    output reg                          CFGGB_val               , // level
+input CCUCTR_Rst
+
+input CCUCTR_CfgVld
+output CTRCCU_CfgRdy
+input CCUCTR_CfgMod
+input CCUCTR_CfgNip
+input CCUCTR_CfgNop
+input CCUCTR_CfgK  
+
+output CTRGLB_CrdAddr    
+output CTRGLB_CrdAddrVld 
+input  GLBCTR_CrdAddrRdy 
+input  GLBCTR_Crd        
+input  GLBCTR_CrdVld     
+output GLBCTR_CrdRdy     
+output CTRGLB_DistAddr   
+output CTRGLB_DistAddrVld
+input  CTRGLB_DistAddrRdy
+input  GLBCTR_Dist       
+input  GLBCTR_DistVld    
+output GLBCTR_DistRdy    
+output CTRGLB_Idx        
+output CTRGLB_IdxVld     
+input  CTRGLB_IdxRdy     
 
 );
 //=====================================================================================================================
@@ -47,18 +69,18 @@ reg [ 3 -1:0 ]state;
 reg [ 3 -1:0 ]next_state;
 always @(*) begin
     case ( state )
-        IDLE : if( ASICCCU_start)
-                    next_state <= CFG; //A network config a time
+        IDLE : if( )
+                    next_state <= CP; //A network config a time
                 else
                     next_state <= IDLE;
-        CFG: if( fifo_full)
-                    next_state <= CMP;
+        CP: if( CTRGLB_CrdVld & GLBCTR_CrdAddrRdy)
+                    next_state <= LP;
                 else
                     next_state <= CFG;
-        CMP: if( all_finish) /// CMP_FRM CMP_PAT CMP_...
-                    next_state <= IDLE;
+        LP: if( CTRPSS_LopLast) /// CMP_FRM CMP_PAT CMP_...
+                    next_state <= CP;
                 else
-                    next_state <= CMP;
+                    next_state <= LP;
         default: next_state <= IDLE;
     endcase
 end
@@ -75,25 +97,93 @@ end
 //=====================================================================================================================
 
 
+always @(posedge clk or rst_n) begin
+    if(!rst_n) begin
+        
+    end else if (GLBCTR_CrdVld & GLBCTR_CrdRdy & state == CP) begin
+        CpCrd <= GLBCTR_Crd;
+    end
+end
+
+always @(posedge clk or rst_n) begin
+    if(!rst_n) begin
+        
+    end else if (GLBCTR_CrdVld & GLBCTR_CrdRdy & state == LP) begin
+        LopCrd <= GLBCTR_Crd;
+    end
+end
+
+assign CTRGLB_CrdAddr = state == CP ? CTRPSS_CpIdx : CTRPSS_LopIdx;
+assign CTRGLB_CrdVld = state == CP | state == LP;
 
 //=====================================================================================================================
 // Sub-Module :
 //=====================================================================================================================
 
-fifo #(
-    .DATA_WIDTH(PORT_WIDTH ),
-    .ADDR_WIDTH(FIFO_ADDR_WIDTH )
-    ) fifo_CONFIG(
-    .clk ( clk ),
-    .rst_n ( rst_n ),
-    .Reset ( 1'b0), 
-    .push(fifo_push) ,
-    .pop(fifo_pop ) ,
-    .data_in( IFCFG_data),
-    .data_out (fifo_out ),
-    .empty(fifo_empty ),
-    .full (fifo_full )
-    );
+PSS#(
+    .SORT_LEN_WIDTH  ( 5 ),
+    .IDX_WIDTH       ( 10 ),
+    .DIST_WIDTH      ( 17 ),
+    .NUM_SORT_CORE   ( 8 ),
+    .SRAM_WIDTH      ( 256 )
+)u_PSS(
+    .CTRPSS_LopLast  ( CTRPSS_LopLast  ),
+    .CTRPSS_Rst      ( CTRPSS_Rst      ),
+    .CTRPSS_Mask     ( CTRPSS_Mask     ),
+    .CTRPSS_MaskVld  ( CTRPSS_MaskVld  ),
+    .PSSCTR_MaskRdy  ( PSSCTR_MaskRdy  ),
+    .CTRPSS_CpIdx    ( CTRPSS_CpIdx    ),
+    .CTRPSS_Lop      ( CTRPSS_Lop      ),// {idx, dist}
+    .CTRPSS_LopVld   ( CTRPSS_LopVld   ),
+    .PSSCTR_LopRdy   ( PSSCTR_LopRdy   ),
+    .PSSCTR_Idx      ( CTRGLB_Idx      ),
+    .PSSCTR_IdxVld   ( CTRGLB_IdxVld   ),
+    .PSSCTR_IdxRdy   ( CTRGLB_IdxRdy   )
+);
+
+counter#(
+    .COUNT_WIDTH ( 3 )
+)u0_counter_CTRPSS_CpIdx(
+    .CLK       ( clk       ),
+    .RESET_N   ( rst_n   ),
+    .CLEAR     ( CCUCTR_Rst     ),
+    .DEFAULT   ( 0   ),
+    .INC       ( INC       ),
+    .DEC       ( 1'b0       ),
+    .MIN_COUNT ( 0 ),
+    .MAX_COUNT ( CCUCTR_CfgNip ),
+    .OVERFLOW  (   ),
+    .UNDERFLOW (  ),
+    .COUNT     ( CTRPSS_CpIdx     )
+);
+assign INC = CTRPSS_LopLast;
+
+counter#(
+    .COUNT_WIDTH ( 3 )
+)u1_counter_LopIdx(
+    .CLK       ( clk       ),
+    .RESET_N   ( rst_n   ),
+    .CLEAR     ( INC     ),
+    .DEFAULT   ( 0   ),
+    .INC       ( INC_LopIdx       ),
+    .DEC       ( 1'b0       ),
+    .MIN_COUNT ( 0 ),
+    .MAX_COUNT ( CCUCTR_CfgNip ),
+    .OVERFLOW  ( CTRPSS_LopLast  ),
+    .UNDERFLOW (  ),
+    .COUNT     ( CTRPSS_LopIdx     )
+);
+assign INC_LopIdx = CTRPSS_LopVld & CTRGLB_IdxRdy;
+
+
+EDC#(
+    .CRD_WIDTH ( 16 ),
+    .CRD_DIM   ( 3 )
+)u_EDC(
+    .Crd0      ( CpCrd      ),
+    .Crd1      ( LopCrd      ),
+    .DistSqr   ( LopDist   )
+);
 
 
 endmodule
