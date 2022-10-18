a0eb54edb1deb6eacd76b1b54c59be88b3cf7ce9
zhouchch3 <2835079913@qq.com>
2022-10-19 06:23:44 +0800

    feat(ARB): Add arb_port output

--- hardware/src/primitives/ARB/prior_arb.v
+++ hardware/src/primitives/ARB/prior_arb.v
@@ -1,8 +1,21 @@
 module prior_arb #(
   parameter REQ_WIDTH = 16
 ) (
-  input  [REQ_WIDTH-1:0]     req,
-  output [REQ_WIDTH-1:0]     gnt
+  input     [REQ_WIDTH          -1 : 0] req,
+  output    [REQ_WIDTH          -1 : 0] gnt,
+  output reg[$clog2(REQ_WIDTH)  -1 : 0] arb_port
 );
-  assign gnt = req & (~(req-1));
+
+assign gnt = req & (~(req-1));
+
+integer i;
+always @(*) begin
+    arb_port = 0;
+    for(i=0; i<REQ_WIDTH; i=i+1) begin
+        if(gnt[i]) begin
+            arb_port |= i;
+        end
+    end
+end
+
 endmodule
