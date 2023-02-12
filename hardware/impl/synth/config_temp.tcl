19d57464f22eb546e6c470efe692f9cf65900c41
zhouchch3 <2835079913@qq.com>
2023-02-13 06:34:22 +0800

    feat(config_temp.tcl): rm

--- hardware/impl/synth/config_temp.tcl
+++
@@ -1,8 +0,0 @@
-set DESIGN_NAME TOP
-set clk I_SysClk
-set PERIOD 2
-set DATE_VALUE 230213
-set TECH_SETTING tech_settings.tcl
-set SDC_FILE ./TOP.sdc
-set SYNTH_PROJDIR ../../work/synth/TOP/Date230213_Period2_group_Track3vt_Notev2
-set UNGROUP none
