4daf1d478c8e6b83e78587f984f1e43dcc11c302
zhouchch3 <2835079913@qq.com>
2023-03-26 21:11:56 +0800

    fix(TOP_tb.v): Add ISA ovf to control state and DatVld

--- hardware/vrf/TOP/TOP_tb.v
+++ hardware/vrf/TOP/TOP_tb.v
@@ -13,7 +13,7 @@ module TOP_tb();
 parameter PORT_WIDTH        = 128;
 parameter ADDR_WIDTH        = 16;
 parameter DRAM_ADDR_WIDTH   = 32;
-parameter OPNUM = 27;
+parameter OPNUM = 6;
 
 //=====================================================================================================================
 // Variable Definition :
@@ -41,8 +41,10 @@ reg [ADDR_WIDTH         -1 : 0] ReqNum;
 
 wire [$clog2(OPNUM)     -1 : 0] ArbCfgRdyIdx;
 reg  [$clog2(OPNUM)     -1 : 0] ArbCfgRdyIdx_d;
-wire [OPNUM -1 : 0][ADDR_WIDTH      -1 : 0] CntMduISARdAddr;
+wire [OPNUM -1 : 0][ADDR_WIDTH      -1 : 0] MduISARdAddr;
+wire [OPNUM -1 : 0] Overflow_ISA;
 reg  [OPNUM -1 : 0][DRAM_ADDR_WIDTH -1 : 0] MDUISABASEADDR;
+reg  [OPNUM -1 : 0][DRAM_ADDR_WIDTH -1 : 0] MDUISANUM;
 wire [OPNUM             -1 : 0] O_CfgRdy;
 wire                            I_ISAVld;
 //=====================================================================================================================
@@ -80,9 +82,10 @@ end
 //=====================================================================================================================
 localparam IDLE     = 3'b000;
 localparam FET      = 3'b001;
-localparam CMD      = 3'b010;
-localparam IN2CHIP  = 3'b011;
-localparam OUT2OFF  = 3'b100;
+localparam WAITCFG  = 3'b010;
+localparam CMD      = 3'b011;
+localparam IN2CHIP  = 3'b100;
+localparam OUT2OFF  = 3'b101;
 
 
 reg [ 3     -1 : 0] state       ;
@@ -96,10 +99,14 @@ always @(*) begin
                 else
                     next_state <= IDLE;
         // ISA
-        FET:    if( !O_CfgRdy[ArbCfgRdyIdx_d] )
-                    next_state <= IDLE;
+        FET:    if( Overflow_ISA )
+                    next_state <= WAITCFG;
                 else
                     next_state <= FET;
+        WAITCFG:    if ( !O_CfgRdy[ArbCfgRdyIdx_d] )
+                    next_state <= IDLE;
+                else
+                    next_state <= WAITCFG;        
         // Data
         CMD :   if( O_DatOE & IO_DatVld & OI_DatRdy) begin
                     if ( IO_Dat[0] ) // 
@@ -136,8 +143,14 @@ MDUISABASEADDR[1] <= 1;
 MDUISABASEADDR[2] <= 17;
 MDUISABASEADDR[3] <= 19;
 MDUISABASEADDR[4] <= 21;
-MDUISABASEADDR[5] <= 26;
-MDUISABASEADDR[6] <= 27;
+MDUISABASEADDR[5] <= 27;
+
+MDUISANUM[0] <= 1;
+MDUISANUM[1] <= 16;
+MDUISANUM[2] <= 2;
+MDUISANUM[3] <= 2;
+MDUISANUM[4] <= 6;
+MDUISANUM[5] <= 1;
 
 end
 
@@ -151,27 +164,55 @@ prior_arb#(
 
 genvar gv_i;
 generate
-    for(gv_i = 0; gv_i < OPNUM; gv_i = gv_i + 1) begin: GEN_CntMduISARdAddr
+    for(gv_i = 0; gv_i < OPNUM; gv_i = gv_i + 1) begin: GEN_MduISARdAddr
+        reg [ADDR_WIDTH     -1 : 0] MduISARdAddr_r; // Last
         wire [ADDR_WIDTH     -1 : 0] MaxCnt = 2**ADDR_WIDTH -1;
+        wire [ADDR_WIDTH     -1 : 0] Default= MDUISABASEADDR[gv_i];
         counter#(
             .COUNT_WIDTH ( ADDR_WIDTH )
-        )u_counter_CntMduISARdAddr(
+        )u_counter_MduISARdAddr(
             .CLK       ( clk            ),
             .RESET_N   ( rst_n          ),
             .CLEAR     ( 1'b0           ),
-            .DEFAULT   ( {ADDR_WIDTH{1'b0}}),
+            .DEFAULT   ( Default        ),
             .INC       ( I_ISAVld & (IO_DatVld & OI_DatRdy) & (ArbCfgRdyIdx_d == gv_i) ),
             .DEC       ( 1'b0           ),
             .MIN_COUNT ( {ADDR_WIDTH{1'b0}}),
             .MAX_COUNT ( MaxCnt         ),
             .OVERFLOW  (                ),
             .UNDERFLOW (                ),
-            .COUNT     ( CntMduISARdAddr[gv_i])
+            .COUNT     ( MduISARdAddr[gv_i])
         );
+        always @(posedge clk or rst_n) begin
+            if (!rst_n) begin
+                MduISARdAddr_r <= 0;
+            end else if(state == IDLE) begin
+                MduISARdAddr_r <= MduISARdAddr[gv_i];
+            end
+        end
+        assign Overflow_ISA[gv_i] = MduISARdAddr[gv_i] - MduISARdAddr_r == MDUISANUM[ArbCfgRdyIdx_d];
+
     end
 endgenerate
 
-assign I_ISAVld = state == FET;
+// wire [ADDR_WIDTH     -1 : 0] MaxCnt = MDUISANUM[ArbCfgRdyIdx_d];
+// counter#(
+//     .COUNT_WIDTH ( ADDR_WIDTH )
+// )u_counter_CntISA(
+//     .CLK       ( clk            ),
+//     .RESET_N   ( rst_n          ),
+//     .CLEAR     ( 1'b0           ),
+//     .DEFAULT   ( {ADDR_WIDTH{1'b0}}),
+//     .INC       ( I_ISAVld & (IO_DatVld & OI_DatRdy) ),
+//     .DEC       ( 1'b0           ),
+//     .MIN_COUNT ( {ADDR_WIDTH{1'b0}}),
+//     .MAX_COUNT ( MaxCnt         ),
+//     .OVERFLOW  (                ),
+//     .UNDERFLOW (                ),
+//     .COUNT     ( CntISA         )
+// );
+
+assign I_ISAVld = state == FET | state == WAITCFG;
 always @(posedge clk or rst_n) begin
     if (!rst_n) begin
         ArbCfgRdyIdx_d <= 0;
@@ -204,12 +245,13 @@ end
         end
     end
 `endif
+
 //=====================================================================================================================
 // Logic Design : Interface
 //=====================================================================================================================
 // DRAM READ
-assign IO_DatVld  = I_ISAVld? 1'b1 : (O_DatOE? 1'bz : state== IN2CHIP);
-assign IO_Dat     = I_ISAVld? Dram[MDUISABASEADDR[ArbCfgRdyIdx_d] + CntMduISARdAddr[ArbCfgRdyIdx_d]] : (O_DatOE? {PORT_WIDTH{1'bz}} : Dram[addr]);
+assign IO_DatVld  = I_ISAVld? state == FET & next_state != WAITCFG : (O_DatOE? 1'bz : state== IN2CHIP);
+assign IO_Dat     = I_ISAVld? Dram[MduISARdAddr[ArbCfgRdyIdx_d]] : (O_DatOE? {PORT_WIDTH{1'bz}} : Dram[addr]);
 
 // DRAM WRITE
 assign OI_DatRdy = I_ISAVld? 1'bz : (O_DatOE? O_CmdVld & state==CMD | !O_CmdVld & state==OUT2OFF: 1'bz);
