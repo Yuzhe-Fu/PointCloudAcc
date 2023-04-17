3b2c2032732993df2fa967bfc9dc8c22da6c7211
zhouchch3 <2835079913@qq.com>
2023-04-17 20:11:41 +0800

    feat(synth-sim,run, PostSim.f): Add muliplt vt

--- hardware/vrf/TOP/PostSim.f
+++ hardware/vrf/TOP/PostSim.f
@@ -1,10 +1,12 @@
 ./TOP_tb.v
 ../../src/primitives/COUNTER/counter.v
-../../work/synth/TOP/Date230413_Period10_group_Track3vt_NoteWithIO_I_SysClk_PAD/p+r_enc/TOP_synth.v
+../../work/synth/TOP/Date230416_Period10_group_Track3vt_NoteWoPAD/p+r_enc/TOP_synth.v
 ../../project/lib/mem/ts1n28hpcpuhdhvtb128x256m1sso_170a/VERILOG/ts1n28hpcpuhdhvtb128x256m1sso_170a_ssg0p81v125c.v
 ../../project/lib/mem/ts1n28hpcpuhdhvtb64x128m4sso_170a/VERILOG/ts1n28hpcpuhdhvtb64x128m4sso_170a_ssg0p81v125c.v
 ../../project/lib/mem/ts1n28hpcpuhdhvtb16x8m2sso_170a/VERILOG/ts1n28hpcpuhdhvtb16x8m2sso_170a_ssg0p81v125c.v
 
 /materials/technology/tsmc28/TSMC_28_IP/STD_CELL/tcbn28hpcplusbwp7t30p140_190a/tcbn28hpcplusbwp7t30p140_110a_vlg/digital/Front_End/verilog/tcbn28hpcplusbwp7t30p140_110a/tcbn28hpcplusbwp7t30p140.v
+/materials/technology/tsmc28/TSMC_28_IP/STD_CELL/tcbn28hpcplusbwp7t30p140hvt_190a/tcbn28hpcplusbwp7t30p140hvt_110a_vlg/digital/Front_End/verilog/tcbn28hpcplusbwp7t30p140hvt_110a/tcbn28hpcplusbwp7t30p140hvt.v
+/materials/technology/tsmc28/TSMC_28_IP/STD_CELL/tcbn28hpcplusbwp7t30p140uhvt_190a/tcbn28hpcplusbwp7t30p140uhvt_140a_vlg/digital/Front_End/verilog/tcbn28hpcplusbwp7t30p140uhvt_140a/tcbn28hpcplusbwp7t30p140uhvt.v
 
 /materials/technology/tsmc28/TSMC_28_IP/STD_IO/tphn28hpcpgv18_170d/tphn28hpcpgv18_110a_vlg/digital/Front_End/verilog/tphn28hpcpgv18_110a/tphn28hpcpgv18.v
