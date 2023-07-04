68de5bf6d12184c8bf86a9be7595467c91608faa
zhouchch3 <2835079913@qq.com>
2023-07-04 19:43:34 +0800

    feat(Dram.txt, test.tcl): Modify using real FPS data

--- hardware/vrf/TOP/test.tcl
+++ hardware/vrf/TOP/test.tcl
@@ -1,4 +1,4 @@
 # stop -condition { #EnTcf == 1'b0 }
-dumptcf -scope TOP_tb.u_TOP -output tcf_period10_Static.dump -overwrite
+dumptcf -scope TOP_tb.u_TOP -output tcf_period10_0703.dump -overwrite
 run
 dumptcf -end
